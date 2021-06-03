f975e15d804c01cd97c2582a08c8945b4d00e096
Kirti Sharma <kirtivats1409@gmail.com>
2021-06-03 14:05:42 +0530

    final project

--- todo.py
+++ todo.py
@@ -1,18 +1,21 @@
-print('``````````````` type "help" to know the commands ```````````````')
+print("-------------------------------------------")
+print("MANAGE ALL YOUR TASKS AND BE MORE ORGANIZED")
+print("-------------------------------------------")
+print()
+print('type "help" to know the commands')
 
 start = 1
-
 
 def help():
 	print("Usage :-")
 
 	print('add "todo item"	# Add a new todo')
 	print('ls		# Show remaining todos')
-	print('del Number	# Delete a todo')
-	print('done Number	# Complete a todo')
+	print('del "NUMBER"	# Delete a todo')
+	print('done "NUMBER"	# Complete a todo')
 	print('help		# Show usage')
 	print('report		# Statistics')
-	print('close 		# Application will be closed')
+	print('close 		# Close the application')
 
 def report():
 	f = open("Pending.txt", "r")
@@ -39,28 +42,55 @@ def ls():
 	f.close()
 
 def delete(x):
-	f = open("Pending.txt", "r")
-	k = f.readlines()
-	p = k.pop(int(x[4::])-1)
-	f.close()
+	msg = "check"
+	take = 'nothing'
+
+	try:
+		item = int(x)
+	except:
+		msg = "** Not a valid command **"
+
+	if msg[0]=='*':
+		pass
 	
-	f2 = open("Pending.txt", "w")
-	for i in k:
-		f2.write(i)
-	f2.close()
+	else:
+		f = open("Pending.txt", "r")
+		k = f.readlines()
 
-	return p
+		try:
+			take = k.pop(item-1)
+			f.close()
+		
+			f2 = open("Pending.txt", "w")
+			for i in k:
+				f2.write(i)
+			f2.close()
+			
+			msg = "Deleted todo #"+x
+		
+		except:
+			msg = "Error: todo #"+x+" does not exist. Nothing deleted."
+			f.close()
 
+	return([msg, take])
+				
 def done(x):
 	p = delete(x)
 	
-	f1 = open("Completed.txt", "a")
-	f1.write(p)
-	f1.close()
+	if p[0][0]=='E':
+		msg = "Error: todo #"+x+" does not exist."
+	
+	elif p[0][0]=='*':
+		msg = p[0]
 
-	print("Maked todo #"+x[5::]+" as done")
+	else:
+		f1 = open("Completed.txt", "a")
+		f1.write(p[1])
+		f1.close()
 
+		msg = "Maked todo #"+x+" as done"
 
+	print(msg)
 
 while start:
 	print()
@@ -72,19 +102,26 @@ while start:
 	elif x=="report":
 		report()
 
-	elif x[0:3]=="add":
+	elif x[0:4]=="add ":
 		add(x)
 
 	elif x=="ls":
 		ls()
 		
-	elif x[0:4]=="done":
-		done(x)
+	elif x[0:5]=="done ":
+		done(x[5::])
 
-	elif x[0:3]=="del":
-		p = delete(x)
-		print("Deleted todo #"+x[4::])
+	elif x[0:4]=="del ":
+		result = delete(x[4::])
+		print(result[0])
 
 	elif x=="close":
-		print("``````````````` Application is closing ```````````````")
-		start = 0
+		print('Are you sure, you want to close the application?\nPress "enter" to confirm\nPress "any key" to decline')
+		decide = input()
+		if len(decide)>0:
+			pass
+		else:
+			start = 0
+
+	else:
+		print("** Not a valid command **")
